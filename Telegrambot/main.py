d1def9ebf32a8e4e282189b73656ced809fca2ef
Mykhailo-lakus <85321827+Mykhailo-lakus@users.noreply.github.com>
2024-06-11 18:47:40 +0000

    update_kursova

--- Telegrambot/main.py
+++ Telegrambot/main.py
@@ -1,60 +1,48 @@
 import requests
 from bs4 import BeautifulSoup
-from telegram import Update
-from telegram.ext import Updater, CommandHandler, CallbackContext, ApplicationBuilder
 from os import environ
 import logging
+import telethon
+from telethon import TelegramClient, events
 
 FORMAT = '%(asctime)-15s %(name)s %(levelname)s: %(message)s'
 logging.basicConfig(level=logging.INFO, format=FORMAT)
 logging.basicConfig(level=logging.ERROR, format=FORMAT)
 logger = logging.getLogger()
 
-# Функція для парсингу сайту
+api_id = 24223475
+
+api_hash = '0f0c353cd431fa23d2e983cb792add8e'
+
+bot_token = environ.get("MY_TG_BOT_TOKEN","define me")
+
+client = TelegramClient('theker', api_id, api_hash).start(bot_token=bot_token)
+
+
+@client.on(events.NewMessage(pattern='/start'))
+async def start_command(event):
+    await client.send_message(event.sender_id, 'Hello, I am a bot that provides random facts. Use /randomfact to get a fact.')
+
+@client.on(events.NewMessage(pattern='/randomfact'))
+async def fact_command(event):
+    text = get_random_fact()
+    print(text)
+    await client.send_message(event.sender_id, text)
+
 def get_random_fact():
     url = 'https://www.randomfunfacts.com/'
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
+
+    fact_div = soup.find('i')
     
-    # Виведемо HTML-код для перевірки (тимчасово)
-    # print(soup.prettify())
-    
-    # Знайдемо елемент, який містить факт
-    fact_div = soup.find('i')  # Вибираємо елемент 'i' (курсивний текст) де містяться факти
-    
-    # Додамо обробку випадку, коли елемент не знайдено
     if not fact_div:
         return "Не вдалося знайти випадковий факт на сайті."
     
     fact = fact_div.get_text(strip=True)
     return fact
 
-# Функція для команди /randomfact
-def random_fact(update: Update, context: CallbackContext) -> None:
-    fact = get_random_fact()
-    update.message.reply_text(f'Random fact: {fact}')
 
-# Функція для команди /start
-def start(update: Update, context: CallbackContext) -> None:
-
-    update.message.reply_text('Hello, I am a bot that provides random facts. Use /randomfact to get a fact.')
-
-def main():
-    # Ваш токен Telegram-бота
-    token =environ.get("MY_TG_BOT_TOKEN","define me")
-    application = ApplicationBuilder().token(token).build()
-
-    application.add_handler(CommandHandler("start", start))
-    
-
-    # Запускаємо бота
-    
-
-    
-    application.add_handler(CommandHandler("randomfact", random_fact))
-
-    application.run_polling()
-
-if __name__ == '__main__':
-    main()
+client.start()
+client.run_until_disconnected()
 
